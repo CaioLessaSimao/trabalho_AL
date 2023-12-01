3814e48d3b7e9f85a8c2528b5da945a6e7fb4701
MountSome <caiolessa02@gmail.com>
2023-12-01 17:33:59 -0300

    Cinza e inverter finalizadas

--- funcoes.py
+++ funcoes.py
@@ -1,15 +1,49 @@
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
-def add_list(img1, img2):
-    return [[img1[i][j] + img2[i][j] for j in range(len(img1[0]))]
-            for i in range(len(img1))]
+def menu():
+	print("Escolha uma das opcoes")
+	print("1) Converter rgb para tons de cinza")
+	print("2) Inverter as cores de uma imagem")
+	print("3) Espelhamento vertical")
+	print("4) Espelhamento horizontal")
+	print("5) Redimensionamento")
+	print("0) Sair")
+	print()
+	return int(input("Digite o numero da sua resposta: "))
 
-def channel_first(img):
-    return [[[img[j][k][i] for k in range(len(img[0]))]
-             for j in range(len(img))] for i in range(len(img[0][0]))]
 
-def funcao_lista(img1, img2):
+
+
+def cinza(img):
+	temp = channel_first(img)
+	return soma_lista(temp[2], soma_lista(temp[0], temp[1]))
+
+def negative(img):
+	aux = []
+	for i in img:
+		aux2 = []
+		for j in i:
+			aux3 = []
+			for k in j:
+				aux3.append(255-k)
+			aux2.append(aux3)
+		aux.append(aux2)
+	return aux
+
+def compare(original, manipulated, title_1="Original", title_2="Manipulated"):
+    plt.figure(figsize=(15, 25))
+    plt.subplot(1, 2, 1)
+    plt.title(title_2)
+    plt.imshow(manipulated)
+    plt.subplot(1, 2, 2)
+    plt.title(title_1)
+    plt.imshow(original)
+    plt.show()
+    return
+
+def soma_lista(img1, img2):
 	aux = []
 	for i in range(len(img1)):
 		aux2 = []
@@ -18,7 +52,7 @@ def funcao_lista(img1, img2):
 		aux.append(aux2)
 	return aux
 
-def funcao_canal(img):
+def channel_first(img):
 	aux = []
 	for i in range(len(img[0][0])):
 		aux2 = []
@@ -29,10 +63,3 @@ def funcao_canal(img):
 			aux2.append(aux3)
 		aux.append(aux2)
 	return aux
-
-
-
-def negative():
-
-
-def cinza():
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 '''Vocês precisam instalar o opencv e o matplotlib na maquina de vocês '''
 import cv2 
 import matplotlib.pyplot as plt
+from funcoes import *
+
 
 def main():
 	image = cv2.imread("lulu.jpg")
 	img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 	img = img.tolist()
 
-	plt.imshow(img)
-	plt.show()
+	m = menu()
+	while(m != 0):
+		if m == 1:
+			plt.imshow(cinza(img), cmap="gray")
+			plt.show()
+		elif m == 2:
+			compare(img, negative(img))
+		m = menu()
 
-	print("Jorge")
 	return
 
 if __name__ == "__main__":
